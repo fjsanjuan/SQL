8107d7a06927e974372a40e7a879d43054ba0c46
fsanjuan <fsanjuan@intelisis.com>
2022-01-24 18:38:43 -0600

    REFACTOR: cambio scripts seguimiento tecnicos

--- Service-Exellence/5 Seguimiento Técnicos/Seguimiento Técnicos Instalación/SQL/Vistas/seguimientoTecnicosWeb.sql
+++ Service-Exellence/5 Seguimiento Técnicos/Seguimiento Técnicos Instalación/SQL/Vistas/seguimientoTecnicosWeb.sql
@@ -1,3 +1,6 @@
+IF EXISTS (SELECT * FROM SYSOBJECTS WHERE ID = OBJECT_ID('dbo.seguimientoTecnicosWeb') AND Type = 'V')
+  DROP VIEW dbo.seguimientoTecnicosWeb
+GO
 CREATE VIEW [dbo].[seguimientoTecnicosWeb] AS 
 SELECT
 Venta.ID,
@@ -24,7 +27,7 @@ VentaD.Precio,
 VentaD.DescuentoLinea,
 VentaD.DescripcionExtra,
 VentaD.Agente,
-SeguimientoOperaciones.Estado,
+dbo.fnCA_estadoOpServicio(venta.id,Ventad.Renglon,Ventad.RenglonID) AS Estado,
 VentaD.CCFechaIni,
 VentaD.CCFechaFin,
 VentaD.CCTiempoTab,
@@ -39,61 +42,5 @@ FROM Venta
 JOIN VentaD ON Venta.ID=VentaD.ID AND VentaD.Cantidad > ISNULL(VentaD.CantidadCancelada, 0)
 JOIN Art ON VentaD.Articulo=Art.Articulo
 LEFT OUTER JOIN Agente AgenteDetalle ON VentaD.Agente=AgenteDetalle.Agente
-LEFT JOIN SeguimientoOperaciones ON SeguimientoOperaciones.idVenta = VentaD.id AND SeguimientoOperaciones.RenglonId = VentaD.RenglonId
 
-WHERE
-	SeguimientoOperaciones.ID = (SELECT TOP  1  id  FROM SeguimientoOperaciones  WHERE IdVenta = VentaD.ID  AND Renglon = VentaD.Renglon  AND  RenglonId = VentaD.RenglonID ORDER BY Id DESC)
-	
-
-UNION ALL
-
-	
-SELECT  
-Venta.ID,
-Venta.Empresa,
-Venta.MovID,
-Venta.Estatus,
-Venta.Situacion,
-Venta.FechaRequerida,
-Venta.HoraRequerida,
-Venta.ServicioArticulo,
-Venta.ServicioSerie,
-Venta.ServicioIdentificador,
-Venta.ServicioPlacas,
-Venta.ServicioNumero,
-Venta.ServicioModelo,
-Venta.Comentarios,
-VentaD.Renglon,
-VentaD.RenglonSub,
-VentaD.RenglonID,
-VentaD.SubCuenta,
-VentaD.Articulo,
-VentaD.Cantidad,
-VentaD.Precio,
-VentaD.DescuentoLinea,
-VentaD.DescripcionExtra,
-VentaD.Agente,
-SeguimientoOperaciones.Estado,
-VentaD.CCFechaIni,
-VentaD.CCFechaFin,
-VentaD.CCTiempoTab,
-Art.Articulo as Articulo1,
-Art.Descripcion1,
-Art.Categoria,
-Art.Tipo,
-AgenteDetalle.Agente as Agente1,
-AgenteDetalle.Nombre,
-Venta.ServicioTipoOperacion
-FROM Venta
-JOIN VentaD ON Venta.ID=VentaD.ID AND VentaD.Cantidad > ISNULL(VentaD.CantidadCancelada, 0)
-JOIN Art ON VentaD.Articulo=Art.Articulo
-LEFT OUTER JOIN Agente AgenteDetalle ON VentaD.Agente=AgenteDetalle.Agente
-LEFT JOIN SeguimientoOperaciones ON SeguimientoOperaciones.idVenta = VentaD.id AND SeguimientoOperaciones.RenglonId = VentaD.RenglonId
-
-WHERE
-	SeguimientoOperaciones.id = NULL
-
-
-
-
-
+GO
